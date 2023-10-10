58f88ffbac65249d0ad27f894be67cb564d06af3
Alejandro <alejandrosanrod@outlook.com>
2023-10-10 10:26:34 +0200

    TDD paso 3, refactorizo código

--- Sesion3/src/pkg/Calculadora.java
+++ Sesion3/src/pkg/Calculadora.java
@@ -3,23 +3,21 @@ package pkg;
 public class Calculadora {
 
 	public static int suma(int i, int j) {
-		return 10;
-		//return i+j;
+		return i+j;
 	}
 
 	public static int resta(int i, int j) {
-		return 0;
-		//return i-j;
+		return i-j;
 	}
 
 	public static int multiplica(int i, int j) {
-		return 25;
-		//return i*j;
+		return i*j;
 	}
 
 	public static int divide(int i, int j) {
-		return 1;
-		//return i/j;
+		if(j!=0)
+			return i/j;
+		else return -1;
 	}
 
 }
